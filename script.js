84e4e0461c124d8a4b105e4e5ac19177fa4da07b
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-04 21:53:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', (e) => {
+  const status = document.querySelector('#status')
+  const enterBtn = document.querySelector('#enterBtn')
+
+  enterBtn.addEventListener('click', () => {
+    status.innerHTML = `<h1>Entered Metaverse</h1>`
+  })
+})
